c3475a1a5e30a67611890201b1738c83f5208df1
SergeyVovchenko <serega.vovchenko@gmail.com>
2024-05-22 10:48:29 +0200

    start-2

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
 
-iziToast.show({
-  title: 'Hey',
-  message: 'What would you like to add?',
-});
+// iziToast.show({
+//   title: 'Hey',
+//   message: 'What would you like to add?',
+// });
 
-// !==========================================================================
+// // !==========================================================================
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
-var lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
-});
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   /* options */
+// });
